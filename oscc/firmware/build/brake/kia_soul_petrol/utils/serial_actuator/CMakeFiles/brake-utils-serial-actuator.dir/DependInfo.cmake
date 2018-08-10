7ef4b2dacfd30eac378532185f1bfe5278ab12c3
rg4444 <rihards.gailums@gmail.com>
2018-08-09 18:04:57 -0700

    2ndcommit
    2ndcommit

---
+++ oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/arduino_init/arduino_init.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/__/__/common/libs/arduino_init/arduino_init.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/can/oscc_can.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/__/__/common/libs/can/oscc_can.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/mcp_can/mcp_can.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/__/__/common/libs/mcp_can/mcp_can.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/pid/oscc_pid.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/__/__/common/libs/pid/oscc_pid.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/serial/oscc_serial.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/__/__/common/libs/serial/oscc_serial.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/timer/oscc_timer.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/__/__/common/libs/timer/oscc_timer.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/brake/kia_soul_petrol/src/accumulator.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/src/accumulator.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/brake/kia_soul_petrol/src/brake_control.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/src/brake_control.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/brake/kia_soul_petrol/src/communications.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/src/communications.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/brake/kia_soul_petrol/src/globals.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/src/globals.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/brake/kia_soul_petrol/src/helper.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/src/helper.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/brake/kia_soul_petrol/src/init.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/src/init.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/brake/kia_soul_petrol/src/master_cylinder.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/__/__/src/master_cylinder.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/brake/kia_soul_petrol/utils/serial_actuator/src/serial_actuator.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/utils/serial_actuator/CMakeFiles/brake-utils-serial-actuator.dir/src/serial_actuator.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BRAKE_STARTUP_TEST"
+  "DEBUG"
+  "KIA_SOUL"
+  "SERIAL_BAUD=115200"
+  "STEERING_OVERRIDE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../brake/kia_soul_petrol/utils/serial_actuator/../../include"
+  "../common/include"
+  "../common/libs/arduino_init"
+  "../common/libs/mcp_can"
+  "../common/libs/pid"
+  "../common/libs/serial"
+  "../common/libs/can"
+  "../common/libs/timer"
+  "../../api/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/CMakeFiles/mega2560_SPI.dir/DependInfo.cmake"
+  "/srv/oscc-joystick-input/oscc/firmware/build/brake/kia_soul_petrol/CMakeFiles/mega2560_CORE.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ oscc/firmware/build/throttle/CMakeFiles/throttle.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/DAC_MCP49xx/DAC_MCP49xx.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/__/common/libs/DAC_MCP49xx/DAC_MCP49xx.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/arduino_init/arduino_init.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/__/common/libs/arduino_init/arduino_init.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/can/oscc_can.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/__/common/libs/can/oscc_can.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/dac/oscc_dac.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/__/common/libs/dac/oscc_dac.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/mcp_can/mcp_can.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/__/common/libs/mcp_can/mcp_can.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/serial/oscc_serial.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/__/common/libs/serial/oscc_serial.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/common/libs/timer/oscc_timer.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/__/common/libs/timer/oscc_timer.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/throttle/src/communications.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/src/communications.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/throttle/src/globals.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/src/globals.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/throttle/src/init.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/src/init.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/throttle/src/main.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/src/main.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/throttle/src/throttle_control.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/src/throttle_control.cpp.obj"
+  "/srv/oscc-joystick-input/oscc/firmware/throttle/src/timers.cpp" "/srv/oscc-joystick-input/oscc/firmware/build/throttle/CMakeFiles/throttle.dir/src/timers.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BRAKE_STARTUP_TEST"
+  "DEBUG"
+  "KIA_SOUL"
+  "SERIAL_BAUD=115200"
+  "STEERING_OVERRIDE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../throttle/include"
+  "../common/include"
+  "../common/libs/arduino_init"
+  "../common/libs/DAC_MCP49xx"
+  "../common/libs/mcp_can"
+  "../common/libs/serial"
+  "../common/libs/can"
+  "../common/libs/dac"
+  "../common/libs/timer"
+  "../../api/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/srv/oscc-joystick-input/oscc/firmware/build/can_gateway/CMakeFiles/uno_SPI.dir/DependInfo.cmake"
+  "/srv/oscc-joystick-input/oscc/firmware/build/can_gateway/CMakeFiles/uno_CORE.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
